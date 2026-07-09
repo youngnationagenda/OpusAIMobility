365b96ad4bd73ddd0400d732f0727af7e6deb806
youngnationagenda <youngnationagenda@gmail.com>
2026-07-10 01:01:20 +0300

    fix: replace abandoned Android UI libs with maintained alternatives
    - PinView: io.github.chaosleong:pinview:1.4.4 (Maven Central, same class/attrs)
    - MaterialRangeBar -> Material RangeSlider (com.google.android.material)
      - Update fragment_filters.xml: RangeBar -> RangeSlider
      - Update FiltersFragment.java: use RangeSlider.addOnChangeListener API
      - Add range_slider_defaults array resource
    - PageIndicator -> me.relex:circleindicator:2.1.6 (Maven Central)
      - Update fragment_food_home.xml: PageIndicator -> CircleIndicator2
      - Add ci_orange_dot and ci_gray_dot drawables
      - FoodHomeFragment unchanged (attachTo API is identical)
    - Remove all stub Java classes (com/chaos, com/appyvet, com/chahinem)
    - Clean up attrs.xml: remove stubs for replaced/real libraries
    - Update celo-deploy Lambda: Alfajores -> Celo Sepolia (chain 11142220)

--- omniride/apps/customer/app/src/main/java/com/terraai/aimobility/food/FiltersFragment.java
+++ omniride/apps/customer/app/src/main/java/com/terraai/aimobility/food/FiltersFragment.java
@@ -1,14 +1,13 @@
 package com.terraai.aimobility.food;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
 
-import com.appyvet.materialrangebar.RangeBar;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.slider.RangeSlider;
 import com.terraai.aimobility.Constants;
 import com.terraai.aimobility.Interface.FragmentCallBack;
 import com.terraai.aimobility.R;
@@ -16,84 +15,69 @@ import com.terraai.aimobility.codeclasses.Functions;
 import com.terraai.aimobility.codeclasses.MyPreferences;
 import com.terraai.aimobility.databinding.FragmentFiltersBinding;
 
+import java.util.List;
+
 public class FiltersFragment extends BottomSheetDialogFragment implements View.OnClickListener {
 
     FragmentFiltersBinding binding;
-    String optionSelected ="" , currencySymbol;
-    FragmentCallBack fragmentCallBack ;
-    String minPrice = "0"  , maxPrice = "1000";
+    String optionSelected = "", currencySymbol;
+    FragmentCallBack fragmentCallBack;
+    String minPrice = "0", maxPrice = "1000";
+
     public FiltersFragment(FragmentCallBack fragmentCallBack) {
-            this.fragmentCallBack = fragmentCallBack;
+        this.fragmentCallBack = fragmentCallBack;
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment]
         binding = FragmentFiltersBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
-        currencySymbol = MyPreferences.getSharedPreference(getActivity()).getString(MyPreferences.currencyUnit, Constants.defaultCurrency);
+        currencySymbol = MyPreferences.getSharedPreference(getActivity())
+                .getString(MyPreferences.currencyUnit, Constants.defaultCurrency);
 
         initializeListeners();
 
-        binding.rangeBar.setTickEnd(Float.parseFloat(maxPrice));
-        binding.rangeBar.setRangePinsByValue(Float.parseFloat(minPrice), Float.parseFloat(maxPrice));
+        // Set initial display labels
+        binding.tvPriceRange.setText(currencySymbol + minPrice);
+        binding.tvFinalRange.setText(currencySymbol + maxPrice);
 
-
-        // Sets the display values of the indices
-        binding.rangeBar.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
-            @Override
-            public void onRangeChangeListener(RangeBar rangeBar, int leftPinIndex,
-                                              int rightPinIndex, String leftPinValue, String rightPinValue) {
-                minPrice = leftPinValue;
-                maxPrice = rightPinValue;
-                binding.tvPriceRange.setText(currencySymbol + minPrice);
-                binding.tvFinalRange.setText(currencySymbol + maxPrice);
-            }
-
-            @Override
-            public void onTouchEnded(RangeBar rangeBar) {
-                Log.d("RangeBar", "Touch ended");
-            }
-
-            @Override
-            public void onTouchStarted(RangeBar rangeBar) {
-                Log.d("RangeBar", "Touch started");
-            }
+        // RangeSlider replaces the abandoned appyvet MaterialRangeBar
+        binding.rangeBar.addOnChangeListener((slider, value, fromUser) -> {
+            List<Float> values = slider.getValues();
+            minPrice = String.valueOf(values.get(0).intValue());
+            maxPrice = String.valueOf(values.get(1).intValue());
+            binding.tvPriceRange.setText(currencySymbol + minPrice);
+            binding.tvFinalRange.setText(currencySymbol + maxPrice);
         });
-
 
         return view;
     }
 
     private void initializeListeners() {
-
         binding.applyBtn.setOnClickListener(this);
         binding.backBtn.setOnClickListener(this);
         binding.clearFilterBtn.setOnClickListener(this);
-
         binding.mostPopularBtn.setOnClickListener(this);
         binding.ratingBtn.setOnClickListener(this);
         binding.deliverTimeBtn.setOnClickListener(this);
     }
 
-
     @Override
     public void onClick(View v) {
-
         switch (v.getId()) {
 
             case R.id.applyBtn:
-                    if(!optionSelected.equals("")) {
-                        Bundle bundle = new Bundle();
-                        bundle.putString("sort", optionSelected);
-                        bundle.putString("min_price", minPrice);
-                        bundle.putString("max_price", maxPrice);
-                        fragmentCallBack.onItemClick(bundle);
-                    }else{
-                        Functions.showToast(getActivity() , "Please select sort filter");
-                    }
-                    dismiss();
+                if (!optionSelected.equals("")) {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("sort", optionSelected);
+                    bundle.putString("min_price", minPrice);
+                    bundle.putString("max_price", maxPrice);
+                    fragmentCallBack.onItemClick(bundle);
+                } else {
+                    Functions.showToast(getActivity(), "Please select sort filter");
+                }
+                dismiss();
                 break;
 
             case R.id.backBtn:
@@ -110,18 +94,18 @@ public class FiltersFragment extends BottomSheetDialogFragment implements View.O
                 break;
 
             case R.id.most_popular_btn:
-                   optionSelected = "popular";
-                   selectOption(binding.mostPopularCheck ,  binding.deliveryCheck , binding.ratingCheck);
+                optionSelected = "popular";
+                selectOption(binding.mostPopularCheck, binding.deliveryCheck, binding.ratingCheck);
                 break;
 
             case R.id.rating_btn:
                 optionSelected = "rating";
-                selectOption(binding.ratingCheck ,  binding.deliveryCheck , binding.mostPopularCheck);
+                selectOption(binding.ratingCheck, binding.deliveryCheck, binding.mostPopularCheck);
                 break;
 
             case R.id.deliver_time_btn:
                 optionSelected = "delivery_time";
-                selectOption(binding.deliveryCheck ,  binding.mostPopularCheck , binding.ratingCheck);
+                selectOption(binding.deliveryCheck, binding.mostPopularCheck, binding.ratingCheck);
                 break;
 
             default:
