1557fcf16707790b002044593dbb602d77960cf2
Sonie <sonie@opusaimobility.com>
2026-07-14 09:09:50 +0300

    fix(android-customer): add missing ProgressBarDrawable import to com.yna CircleProgressBarDrawable

--- MobilityAIapp/android/customer/app/src/main/java/com/yna/opusaimobilityapp/codeclasses/CircleProgressBarDrawable.java
+++ MobilityAIapp/android/customer/app/src/main/java/com/yna/opusaimobilityapp/codeclasses/CircleProgressBarDrawable.java
@@ -8,6 +8,8 @@ import android.graphics.PixelFormat;
 import android.graphics.Rect;
 import android.graphics.RectF;
 
+// ProgressBarDrawable lives in com.terraai.aimobility.codeclasses (legacy package)
+import com.terraai.aimobility.codeclasses.ProgressBarDrawable;
 
 public class CircleProgressBarDrawable extends ProgressBarDrawable {
 
